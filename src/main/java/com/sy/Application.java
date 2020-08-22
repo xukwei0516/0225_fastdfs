6160e4d1232453e7003fc4feaf7351bf14cb4001
xukwei0516 <260724109@qq.com>
2020-08-22 17:55:22 +0800

    张三第二次上传

--- src/main/java/com/sy/Application.java
+++ src/main/java/com/sy/Application.java
@@ -12,6 +12,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("张三的编辑");
         System.out.println("李四的编辑");
+        System.out.println("张三的第二次上传");
         SpringApplication.run(Application.class, args);
     }
 
