12df028341578b4df04412a681060e88745fd892
xukwei0516 <35026437+xukwei0516@users.noreply.github.com>
2020-08-22 17:49:09 +0800

    Update Application.java

--- src/main/java/com/sy/Application.java
+++ src/main/java/com/sy/Application.java
@@ -11,6 +11,7 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("张三的编辑");
+        System.out.println("李四的编辑");
         SpringApplication.run(Application.class, args);
     }
 
